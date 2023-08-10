deb4e53129903fa7f596c2f638b3dd5808a5962b
Ivan <ivanaires32@gmail.com>
2023-08-09 21:00:52 -0300

    feat: page activity

--- src/pages/Dashboard/Activities/index.js
+++ src/pages/Dashboard/Activities/index.js
@@ -1,3 +1,16 @@
+import styled from 'styled-components';
+import Typography from '@material-ui/core/Typography';
+import ActivitiesInformations from '../../../components/ActivitiesInformation';
+
 export default function Activities() {
-  return 'Atividades: Em breve!';
+  return (
+    <>
+      <StyledTypography variant="h4">Escolha de atividades</StyledTypography>
+      <ActivitiesInformations />
+    </>
+  );
 }
+
+const StyledTypography = styled(Typography)`
+  margin-bottom: 20px !important;
+`;
